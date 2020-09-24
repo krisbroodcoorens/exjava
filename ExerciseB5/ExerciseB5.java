518691adf63c82a2dd2e316d7f63a348bc6924d9
krisbroodcoorens <kris.broodcoorens@bnpparibasfortis.com>
2020-09-24 15:17:05 +0200

    Exercise B5

--- ExerciseB5/ExerciseB5.java
+++ ExerciseB5/ExerciseB5.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class ExerciseB5
 {
-	enum GreetAgain {Y, N};
+	enum GreetAgain {Y, N, y, n};
 	
 	public static void main(String[] args)
 	{
 		GreetAgain userAnswer = GreetAgain.Y;
 		Scanner readAnswer = new Scanner(System.in);
 		
-		while (userAnswer.toString() == "Y")
+		while (userAnswer.equals(GreetAgain.Y) || userAnswer.equals(GreetAgain.y))
 		{
 			System.out.println("Welcome! Would you like to be greeted again? (Y/N)");
 			userAnswer = GreetAgain.valueOf(readAnswer.next());										
