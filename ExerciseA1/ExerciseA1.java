a7e6349cc0757240682a65d16d28bd14ae34423e
krisbroodcoorens <kris.broodcoorens@bnpparibasfortis.com>
2020-09-24 09:44:55 +0200

    Exercise A1

---
+++ ExerciseA1/ExerciseA1.java
@@ -0,0 +1,13 @@
+public class ExerciseA1
+{
+	public static void main(String[] args)
+	{
+		String courseTitle = "Java Reskilling";
+		int numberDays = 30;
+		double priceDay = 1000.00;
+		boolean priorKnowledge = false;
+		
+		System.out.println("The course " +courseTitle+ " takes " +numberDays+ " days for " +priceDay+ " per day and prior knowledge is " +priorKnowledge+ ".");
+		
+	}	
+}
