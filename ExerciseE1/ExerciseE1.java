9c31b370db39fcedd426f23a6621a8b795785427
krisbroodcoorens <kris.broodcoorens@bnpparibasfortis.com>
2020-09-25 15:01:27 +0200

    Exercise E1

--- ExerciseE1/ExerciseE1.java
+++ ExerciseE1/ExerciseE1.java
@@ -1,9 +1,4 @@
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -16,38 +11,40 @@ public class ExerciseE1
 		int instructorAge;
 		int startAge;
 		double startSalary;		
+		Scanner readInputData = new Scanner(System.in);
 	
 		//Accept value "instructorFirstName" from user via Terminal
-		instructorFirstName = acceptFirstName();		
+		instructorFirstName = acceptFirstName(readInputData);		
 		
 		//Accept value "instructorFirstName" from user via Terminal
-		instructorLastName  = acceptLastName();	
+		instructorLastName  = acceptLastName(readInputData);	
 		
 		//Accept value "instructorFirstName" from user via Terminal
-		instructorAge 		 = acceptAge();
+		instructorAge 		 = acceptAge(readInputData);
 		
 		//Accept value "startAge" from user via Terminal
-		startAge 		 	 = acceptStartAge();
+		startAge 		 	 = acceptStartAge(readInputData);
 		
 		//Accept value "instructorFirstName" from user via Terminal
-		startSalary  		 = acceptStartSalary();
+		startSalary  		 = acceptStartSalary(readInputData);
 		
 		//Calculate salary history
-		printSalaryHistory(instructorFirstName, instructorLastName, instructorAge, startAge, startSalary);		
+		printSalaryHistory(instructorFirstName, instructorAge, startAge, startSalary);		
+		
+		readInputData.close();
 	}
 	
-	public static String acceptFirstName()
+	public static String acceptFirstName(Scanner readInputData)
 	{
 		boolean firstNameOk = false;
-		String firstName = null;
-		Scanner readFirstName = new Scanner(System.in);
+		String firstName = null;		
 		//Accept value "instructorFirstName" from user via Terminal => loop until entered format of "instructorFirstName" is valid
 		do
 		{
 			try
 			{
 				System.out.println("Please enter instructor first name: ");
-				firstName = readFirstName.nextLine();
+				firstName = readInputData.nextLine();
 				System.out.println(firstName);				
 				if (firstName.isEmpty())
 				{
@@ -64,23 +61,21 @@ public class ExerciseE1
 			}
 		} 
 		while (!firstNameOk);
-		
-		readFirstName.close();
+	
 		return firstName;
 	}
 	
-	public static String acceptLastName()
+	public static String acceptLastName(Scanner readInputData)
 	{
 		boolean lastNameOk = false;
 		String lastName = null;
-		Scanner readLastName = new Scanner(System.in);
 		//Accept value "lastName" from user via Terminal => loop until entered format of "lastName" is valid
 		do
 		{
 			try
 			{
 				System.out.println("Please enter instructor last name");
-				lastName = readLastName.nextLine();
+				lastName = readInputData.nextLine();
 				if (lastName.isEmpty())
 				{
 					System.out.println("Instructor last name should be filled in.");
@@ -97,95 +92,92 @@ public class ExerciseE1
 		} 
 		while (!lastNameOk);		
 		
-		readLastName.close();
 		return lastName;		
 	}
 	
-	public static int acceptAge()
+	public static int acceptAge(Scanner readInputData)
 	{
 		boolean ageOk = false;
 		int age = 0;
-		Scanner readAge = new Scanner(System.in);
 		//Accept value "age" from user via Terminal => loop until entered format of "age" is valid
 		do
 		{
 			try
 			{
 				System.out.println("Please enter instructor age");
-				age = Integer.parseInt(readAge.nextLine());
+				age = Integer.parseInt(readInputData.nextLine());
 				ageOk = true;
 			}
-			catch (Exception expAge)
+			catch (NumberFormatException expAge)
 			{
 				System.out.println("Age is not numeric. Please enter the age of the instructor in a correct format.");
 			}
 		} while (!ageOk);
 		
-		readAge.close();
 		return age;
 	}
 		
-	public static int acceptStartAge()
+	public static int acceptStartAge(Scanner readInputData)
 	{
 		boolean startAgeOk = false;
 		int startAge = 0;
-		Scanner readStartAge = new Scanner(System.in);
 		//Accept value "startAge" from user via Terminal => loop until entered format of "startAge" is valid
 		do
 		{
 			try
 			{
 				System.out.println("Please enter instructor startAge");
-				startAge = Integer.parseInt(readStartAge.nextLine());
+				startAge = Integer.parseInt(readInputData.nextLine());
 				startAgeOk = true;
 			}
-			catch (Exception expStartAge)
+			catch (NumberFormatException expStartAge)
 			{
 				System.out.println("Start age is not numeric. Please enter the start age of the instructor in a correct format.");
 			}
 		} while (!startAgeOk);
 		
-		readStartAge.close();
 		return startAge;				
 	}
 	
-	public static double acceptStartSalary()
+	public static double acceptStartSalary(Scanner readInputData)
 	{
 		boolean startSalaryOk = false;
 		double startSalary = 0.0;
-		Scanner readStartSalary = new Scanner(System.in);
 		//Accept value "startSalary" from user via Terminal => loop until entered format of "startSalary" is valid
 		do
 		{
 			try
 			{
 				System.out.println("Please enter instructor start salarty");
-				startSalary = Double.parseDouble(readStartSalary.nextLine());
+				startSalary = Double.parseDouble(readInputData.nextLine());
 				startSalaryOk = true;
 			}
-			catch (Exception expStartSalary)
+			catch (NumberFormatException expStartSalary)
 			{
 				System.out.println("Start Salary is not numeric. Please enter the start salary of the instructor in a correct format.");
 			}
 		} while (!startSalaryOk);
 		
-		readStartSalary.close();
 		return startSalary;		
 	}
 	
-	public static void printSalaryHistory (String firstName, String lastName, int age, int startAge, double startSalary) 
+	public static void printSalaryHistory (String firstName, int age, int startAge, double startSalary) 
 	{
+		int increasedAge = startAge;
+		double increasedSalary = startSalary;
+		int currentAge = age;
+		
 		try 
 		{
-			FileWriter mySalaryHistoryWriter = new FileWriter("salaryHistory.txt");;
-				
-			while (startAge <= age && (startSalary - startAge) <= 35)
+			FileWriter mySalaryHistoryWriter = new FileWriter("salaryHistory.txt");
+			while (increasedAge <= currentAge && (increasedAge - startAge) <= 35 )
 			{
-				mySalaryHistoryWriter.write("Salary of " + firstName + " at " + startAge + " is " + startSalary + "\n");
-				startAge += 5;
-				startSalary *= 1.03;
+				mySalaryHistoryWriter.write("Salary of " + firstName + " at " + increasedAge + " is " + increasedSalary + "\n");
+				increasedAge += 5;
+				increasedSalary *= 1.03;
 			}
-			mySalaryHistoryWriter.write("Maximum Salary reached");				
+			mySalaryHistoryWriter.write("Maximum Salary reached");
+			System.out.println("Successfully create file salaryHistory.txt");
 			mySalaryHistoryWriter.close();			
 		}
 		catch(IOException expSalaryHistory) 
